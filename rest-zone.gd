aa450a0a68686f95d5d07e7847a940f59a2037ad
Phuwit Puthipairoj <phuwitput@gmail.com>
2021-12-07 15:21:43 +0700

    rest zone

---
+++ rest-zone.gd
@@ -0,0 +1,12 @@
+extends Position2D
+
+func _draw():
+	draw_circle(Vector2.ZERO, 75, Color.salmon)
+
+func deselect():
+	modulate = Color.white
+
+func select():
+	for child in get_tree().get_nodes_in_group('zone'):
+		child.deselect()
+	modulate = Color.aquamarine
