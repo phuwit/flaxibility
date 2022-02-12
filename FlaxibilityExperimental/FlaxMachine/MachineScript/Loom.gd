3a3b119f9fc31bed3b784a5722e123b53af6099f
Phuwit Puthipairoj <phuwitput@gmail.com>
2022-02-12 11:08:03 +0700

    remove lerp

--- FlaxibilityExperimental/FlaxMachine/MachineScript/Loom.gd
+++ FlaxibilityExperimental/FlaxMachine/MachineScript/Loom.gd
@@ -6,16 +6,18 @@ var type = "Loom"
 
 var mouse_over = false
 var clicked = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	
 	if (clicked == true) and (mouse_over == true):
-		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+		# global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+		global_position = get_global_mouse_position()
 	# else:
 	# 	global_position = lerp(global_position, rest_point, 10 * delta)
 	# pass
@@ -25,7 +27,7 @@ func _input(event):
 		get_tree().set_input_as_handled()
 		print("clicked")
 		clicked = true
-	elif (event is InputEventMouseButton) and (event.button_index == BUTTON_LEFT) and (event.pressed == false):
+	elif (event is InputEventMouseButton) and (event.pressed == false):
 		clicked = false
 
 func _on_MachineLoom_mouse_entered():
