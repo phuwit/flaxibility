8dc4f7d10504485a4f597fd99c62aae660d5732e
Phuwit Puthipairoj <phuwitput@gmail.com>
2022-02-12 12:53:18 +0700

    experimental: drag and drop works once

--- FlaxibilityExperimental/FlaxMachine/MachineScript/Dyeing.gd
+++ FlaxibilityExperimental/FlaxMachine/MachineScript/Dyeing.gd
@@ -1,49 +1,41 @@
 extends Area2D
 
 
-var cost = 30
-var type = "Loom"
-var shortestDist = 60
+var cost = 40
+var type = "Dyeing"
 
 var mouse_over = false
 var clicked = false
-var restPoint
-var restNodes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	restNodes = get_tree().get_nodes_in_group('restZones')
-	restPoint = restNodes[0].global_position
-	restNodes[0].select()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	
 	if (clicked == true) and (mouse_over == true):
 		# global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
 		global_position = get_global_mouse_position()
-	else:
-		global_position = lerp(global_position, restPoint, 10 * delta)
+	# else:
+	# 	global_position = lerp(global_position, rest_point, 10 * delta)
 	# pass
 
 func _input(event):
 	if (mouse_over == true) and (event is InputEventMouseButton) and (event.button_index == BUTTON_LEFT) and (event.pressed == true):
 		get_tree().set_input_as_handled()
-		print("clicked")
+		# print("clicked")
 		clicked = true
 	elif (event is InputEventMouseButton) and (event.pressed == false):
 		clicked = false
-		for child in restNodes:
-			var distanceToRest  = global_position.distance_to(child.global_position)
-			if distanceToRest < shortestDist:
-				child.select()
-				restPoint = child.global_position
-				shortestDist = distanceToRest
 
 func _on_MachineLoom_mouse_entered():
 	mouse_over = true
-	print("MOUSE OVER LAEW")
+	# print("MOUSE OVER LAEW")
+	pass # Replace with function body.
+
 
 func _on_MachineLoom_mouse_exited():
 	mouse_over = false
-	print("MOUSE EXIT LAEW")
+	# print("MOUSE EXIT LAEW")
