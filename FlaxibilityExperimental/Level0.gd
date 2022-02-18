86823dbf4e1a52153ac06025dcda585912165f16
Krit50322 <Jasada.k04@gmail.com>
2022-02-18 16:13:53 +0700

    Flax exp final

--- FlaxibilityExperimental/Level0.gd
+++ FlaxibilityExperimental/Level0.gd
@@ -35,72 +35,106 @@ func _on_RunButton_pressed():
 		RunButton = false
 	else:
 		RunButton = true
+	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[00]]  and (drawer.get_node("MachineSewing").currentNode == grid.restNodesGrid[grid.restNodesPos[01]] or drawer.get_node("MachineSewing").currentNode == grid.restNodesGrid[grid.restNodesPos[10]]) and drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[11]]:
+		get_node("Warning").visible_characters = 0
+		while cotton >= 5 and RunButton == true:
+			if cotton >= 5:
+				cotton = cotton - 5
+				Global.cotton = cotton
+				fabric += 1
+				Global.fabric = fabric
+				yield(get_tree().create_timer(0.25),"timeout")
+			else:
+				break
+		while fabric >= 1 and color >= 1 and RunButton == true:
+			if fabric >= 1 and color >0:
+				color = color - 1
+				Global.color = color
+				fabric = fabric - 1
+				Global.fabric = fabric
+				color_fabric += 1
+				Global.color_fabric += 1
+				yield(get_tree().create_timer(0.25),"timeout")
+			else:
+				break
+	else:
+		get_node("Warning").visible_characters = -1
+	if fabric == 15 and color_fabric ==5 :
+		gameClear()
+	
 	#transfer system
-	while (cotton > 0) and (RunButton == true):
-		if cotton >= 1:
-			cotton = cotton - 1
-			gridID_Back.get_node("CollisionShape00").cotton += 1 
-			yield(get_tree().create_timer(0.2),"timeout")
-		else:
-			break
-	while (thread > 0) and (RunButton == true):
-		if thread >= 1:
-			thread = thread - 1
-			gridID_Back.get_node("CollisionShape00").thread += 1 
-			yield(get_tree().create_timer(0.2),"timeout")
-		else:
-			break
-	while (fabric > 0) and (RunButton == true):
-		if fabric >= 1:
-			fabric = fabric - 1
-			gridID_Back.get_node("CollisionShape00").fabric += 1 
-			yield(get_tree().create_timer(0.2),"timeout")
-		else:
-			break
-	while (color_fabric > 0) and (RunButton == true):
-		if color_fabric >= 1:
-			color_fabric = color_fabric - 1
-			gridID_Back.get_node("CollisionShape00").color_fabric += 1 
-			yield(get_tree().create_timer(0.2),"timeout")
-		else:
-			break
+#	while (cotton > 0) and (RunButton == true):
+#		if cotton >= 1:
+#			cotton = cotton - 1
+#			gridID_Back.get_node("CollisionShape00").cotton += 1 
+#			yield(get_tree().create_timer(0.2),"timeout")
+#		else:
+#			break
+#	while (thread > 0) and (RunButton == true):
+#		if thread >= 1:
+#			thread = thread - 1
+#			gridID_Back.get_node("CollisionShape00").thread += 1 
+#			yield(get_tree().create_timer(0.2),"timeout")
+#		else:
+#			break
+#	while (fabric > 0) and (RunButton == true):
+#		if fabric >= 1:
+#			fabric = fabric - 1
+#			gridID_Back.get_node("CollisionShape00").fabric += 1 
+#			yield(get_tree().create_timer(0.2),"timeout")
+#		else:
+#			break
+#	while (color_fabric > 0) and (RunButton == true):
+#		if color_fabric >= 1:
+#			color_fabric = color_fabric - 1
+#			gridID_Back.get_node("CollisionShape00").color_fabric += 1 
+#			yield(get_tree().create_timer(0.2),"timeout")
+#		else:
+#			break
 			
 
 			
 
-		
-func updateMachine_pos():
-	#Loom machine update
-	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[00]]: #and grid[0]!= 1:
-		gridID[0] = 0
-	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[01]]: #and grid[1]!= 1:
-		gridID[1] = 0
-	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[10]]: #and grid[2]!= 1:
-		gridID[2] = 0
-	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[11]]: #and grid[3]!= 1:
-		gridID[3] = 0
-	#dyeing machine update
-	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[00]]: #and grid[0]!= 1 :
-		gridID[0] = 1
-	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[01]]: #and grid[1]!= 1:
-		gridID[1] = 1
-	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[10]]: #and grid[2]!= 1:
-		gridID[2] = 1
-	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[11]]: #and grid[3]!= 1:
-		gridID[3] = 1
-	Global.gridID = gridID
-	print(gridID)
-		
-		
-		
-func _on_Timer_timeout():
-	updateMachine_pos()
-	updateResource()
+func gameClear():
+	get_node("GameClearPopup").position = Vector2(960,540)
 
-func updateResource():
-	Global.cotton = cotton +gridID_Back.get_node("CollisionShape00").cotton + gridID_Back.get_node("CollisionShape01").cotton + gridID_Back.get_node("CollisionShape10").cotton + gridID_Back.get_node("CollisionShape11").cotton
-	Global.fabric = fabric + gridID_Back.get_node("CollisionShape00").fabric + gridID_Back.get_node("CollisionShape01").fabric + gridID_Back.get_node("CollisionShape10").fabric + gridID_Back.get_node("CollisionShape11").fabric
-	Global.color_fabric = color_fabric + gridID_Back.get_node("CollisionShape00").color_fabric + gridID_Back.get_node("CollisionShape01").color_fabric + gridID_Back.get_node("CollisionShape10").color_fabric + gridID_Back.get_node("CollisionShape11").color_fabric
+
+
+
+
+
+#func updateMachine_pos():
+#	#Loom machine update
+#	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[00]]: #and grid[0]!= 1:
+#		gridID[0] = 0
+#	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[01]]: #and grid[1]!= 1:
+#		gridID[1] = 0
+#	if drawer.get_node("MachineLoom").currenNode == grid.restNodesGrid[grid.restNodesPos[10]]: #and grid[2]!= 1:
+#		gridID[2] = 0
+#	if drawer.get_node("MachineLoom").currentNode == grid.restNodesGrid[grid.restNodesPos[11]]: #and grid[3]!= 1:
+#		gridID[3] = 0
+#	#dyeing machine update
+#	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[00]]: #and grid[0]!= 1 :
+#		gridID[0] = 1
+#	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[01]]: #and grid[1]!= 1:
+#		gridID[1] = 1
+#	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[10]]: #and grid[2]!= 1:
+#		gridID[2] = 1
+#	if drawer.get_node("MachineDyeing").currentNode == grid.restNodesGrid[grid.restNodesPos[11]]: #and grid[3]!= 1:
+#		gridID[3] = 1
+#	Global.gridID = gridID
+#	print(gridID)
+#
+		
+		
+#func _on_Timer_timeout():
+#	updateMachine_pos()
+#	updateResource()
+
+#func updateResource():
+#	Global.cotton = cotton +gridID_Back.get_node("CollisionShape00").cotton + gridID_Back.get_node("CollisionShape01").cotton + gridID_Back.get_node("CollisionShape10").cotton + gridID_Back.get_node("CollisionShape11").cotton
+#	Global.fabric = fabric + gridID_Back.get_node("CollisionShape00").fabric + gridID_Back.get_node("CollisionShape01").fabric + gridID_Back.get_node("CollisionShape10").fabric + gridID_Back.get_node("CollisionShape11").fabric
+#	Global.color_fabric = color_fabric + gridID_Back.get_node("CollisionShape00").color_fabric + gridID_Back.get_node("CollisionShape01").color_fabric + gridID_Back.get_node("CollisionShape10").color_fabric + gridID_Back.get_node("CollisionShape11").color_fabric
 	
 func grid():
 	var maxgrid = int(gridX*gridY)
